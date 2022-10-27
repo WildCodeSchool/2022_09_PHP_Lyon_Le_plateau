81f58944108b7e2d96b5f3a5c86c7bdd37ad1ef4
Kévin Albespy <kevin.albespy@gmail.com>
2022-10-27 16:59:32 +0200

    Initiation of User Story Add Game

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -13,4 +13,24 @@ class GameController extends AbstractController
 
         return $this->twig->render('Game/index.html.twig', ['games' => $games]);
     }
+
+    public function add(): ?string
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $game = array_map('trim', $_POST);
+
+            // TODO validations (length, format...)
+            // function verification to add //
+
+            // if validation is ok, insert and redirection
+            $gameManager = new GameManager();
+            $id = $gameManager->insert($game);
+
+            header('Location:/games/show?id=' . $id);
+            return null;
+        }
+
+        return $this->twig->render('Game/add.html.twig');
+    }
 }
--- src/Model/GameManager.php
+++ src/Model/GameManager.php
@@ -7,4 +7,24 @@ use PDO;
 class GameManager extends AbstractManager
 {
     public const TABLE = 'game';
+
+    /**
+     * Insert new item in database
+     */
+    public function insert(array $game): int
+    {
+        $query = "INSERT INTO " . self::TABLE .
+        " (id_owner, name, type, min_number_players, max_number_players, minimum_players_age) 
+        VALUES (:id_owner, :name, :type, :min_number_players, :max_number_player :minimum_players_age)";
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue(':id_owner', $game['idGameOwner'], PDO::PARAM_STR);
+        $statement->bindValue(':name', $game['gameName'], PDO::PARAM_STR);
+        $statement->bindValue(':type', $game['gameGenre'], PDO::PARAM_STR);
+        $statement->bindValue(':min_number_players', $game['gameMinNumberPlayers'], PDO::PARAM_INT);
+        $statement->bindValue(':max_number_players', $game['gameMaxNumberPlayers'], PDO::PARAM_INT);
+        $statement->bindValue(':minimum_players_age', $game['gameAgeMinimumPlayers'], PDO::PARAM_INT);
+
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
 }
