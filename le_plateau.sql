2e6dda176c4ecd4f1aa8d9d1f8896bd6e2435a6a
Matthieu Dufloux <matthieu.dufloux.@pro@gmail.com>
2022-10-28 10:06:36 +0200

    delete the column number_players

--- le_plateau.sql
+++ le_plateau.sql
@@ -14,7 +14,6 @@ CREATE TABLE game (
 id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 name VARCHAR(100) NOT NULL,
 type VARCHAR(100) NOT NULL,
-number_players INT NOT NULL,
 minimum_players_age INT NOT NULL,
 image VARCHAR(255),
 id_owner INT NOT NULL,
@@ -31,8 +30,8 @@ VALUES
 ('Ibrahim', 'Mohamed', 'i.mohammed@gmail.com', "ibrabra"),
 ('Matthieu', 'Dufloux', 'm.dufloux@gmail.com', "matmat");
 
-INSERT INTO game (name, type, number_players, minimum_players_age, id_owner, image, min_number_players, max_number_players)
+INSERT INTO game (name, type, minimum_players_age, id_owner, image, min_number_players, max_number_players)
 VALUES
-('Monopoly', 'plateau', 8, 10, 1,'https://img.freepik.com/photos-gratuite/beaucoup-maisons-jouets_144627-1474.jpg?size=626&ext=jpg&ga=GA1.2.839792511.1665667296&semt=sph', 2, 6),
-('Uno', 'cartes', 6, 6, 3, 'https://img.freepik.com/photos-gratuite/homme-jouant-aux-cartes-autres-personnes-tenant-jeu_1268-17890.jpg?size=626&ext=jpg&ga=GA1.2.839792511.1665667296&semt=sph', 2, 8),
-('Belotte', 'cartes', 4, 15, 5, 'https://img.freepik.com/vecteurs-libre/quatre-as-carte-poker-illustration_1017-3850.jpg?size=338&ext=jpg&ga=GA1.2.839792511.1665667296&semt=sph', 4, 4);
+('Monopoly', 'plateau', 10, 1,'https://img.freepik.com/photos-gratuite/beaucoup-maisons-jouets_144627-1474.jpg?size=626&ext=jpg&ga=GA1.2.839792511.1665667296&semt=sph', 2, 6),
+('Uno', 'cartes', 6, 3, 'https://img.freepik.com/photos-gratuite/homme-jouant-aux-cartes-autres-personnes-tenant-jeu_1268-17890.jpg?size=626&ext=jpg&ga=GA1.2.839792511.1665667296&semt=sph', 2, 8),
+('Belotte', 'cartes', 15, 5, 'https://img.freepik.com/vecteurs-libre/quatre-as-carte-poker-illustration_1017-3850.jpg?size=338&ext=jpg&ga=GA1.2.839792511.1665667296&semt=sph', 4, 4);
