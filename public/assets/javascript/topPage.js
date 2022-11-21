ea55609ccb1c328fddcaf0e74edd2cf422e7192e
Grom69 <jerome.dannfald@orange.fr>
2022-11-21 22:19:31 +0100

    merge + udpate scroll JS top_page_button

--- public/assets/javascript/topPage.js
+++ public/assets/javascript/topPage.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
     window.onscroll = function (ev) {
-        document.getElementById("cRetour").className = (window.pageYOffset > 150) ? "cVisible" : "cInvisible";
+        document.getElementById("cRetour").className = (window.pageYOffset > 300) ? "cVisible" : "cInvisible";
     };
 });
